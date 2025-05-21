c92808096a2e7aa85e8d7010930c8c43e6cf86d3
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-05-21 20:46:37 -0300

    atualizacao

---
+++ php/insert_user.php
@@ -0,0 +1,14 @@
+<?php
+include 'conexao.php';
+
+$insert = "INSERT INTO tb_user VALUES (null, 'carol', 'carol@gmail.com', 'Secretaria, '123456')";
+
+$query = ($conexao->query($insert));
+
+if($conexao->query($insert)){
+    echo "<script>alert('Usuário Cadastrado com Sucesso!') location.href = '../cadastro.html'</script>";
+    
+}
+
+
+?>
