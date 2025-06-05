5f940c6b03094ad28481d95286e49eebb7fa75b3
Carol31-a <carolvitoria311007@gmail.com>
2025-06-04 21:22:41 -0300

    04/06
    12334523523fwxczzcxcxzxzfcas fdsa

--- php/insert_user.php
+++ php/insert_user.php
@@ -1,13 +1,27 @@
 <?php
+
+//recebendo os dados do formulário//
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$setor = $_POST['setor'];
+$senha = $_POST['senha'];
+
+
+
+
+// chamando o arquivo de conexão com o banco de dados (é como se eu desse ctrl c e ctrl v no código da conexão.php//
+
 include 'conexao.php';
 
-$insert = "INSERT INTO tb_user VALUES (null, 'carol', 'carol@gmail.com', 'Secretaria, '123456')";
 
-$query = ($conexao->query($insert));
+$insert = "INSERT INTO tb_user VALUES (null, '$nome', '$email', '$setor', '$senha')";
 
-if($conexao->query($insert)){
-    echo "<script>alert('Usuário Cadastrado com Sucesso!') location.href = '../cadastro.html'</script>";
-    
+$query = $conexao ->query($insert);
+
+if ($conexao ->query($insert) === TRUE) {
+    echo "<script>alert('Usuário Cadastrado com Sucesso!');
+    location.href = '../cadastro.html'</script>";
 }
 
 
