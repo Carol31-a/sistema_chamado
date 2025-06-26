86f0790c544241584a19644ad9d05c99bb9f375f
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-06-26 20:58:42 -0300

    update e delete ok

--- php/insert_user.php
+++ php/insert_user.php
@@ -1,28 +1,70 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    
+    <!--Link do Font Awesome-->
+    <script src="https://kit.fontawesome.com/c0f408d1cc.js" crossorigin="anonymous"></script>
 
-//recebendo os dados do formulário//
+    <title>Usuários Cadastrados</title>
 
-$nome = $_POST['nome'];
-$email = $_POST['email'];
-$setor = $_POST['setor'];
-$senha = $_POST['senha'];
+</head>
+<body>
+    <nav class="navbar bg-danger">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+        </div>
+    </nav>
+
+    <div class="container">
+        <h1>Lista de Usuários</h1>
+        <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">ID</th>
+            <th scope="col">NOME</th>
+            <th scope="col">EMAIL</th>
+            <th scope="col">SETOR</th>
+            <th scope="col">DELETE</th>
+            <th scope="col">EDITAR</th>
+            </tr>
+        </thead>
+        <tbody>
+                <!-- PHP -->
+                <?php
+                
+                    // INCLUINDO O ARQUIVO DE CONEXÃO // 
+                    include 'conexao.php';
+
+                    // INSTRUÇÃO DO SQL PARA CAPTURAR TODOS OS DADOS DO BANCO //
+                    $select = "SELECT * FROM tb_user";
+
+                    // FUNÇÃO QUERY EXECUTA O SELECT DENTRO DO BANCO //
+                    $query = $conexao->query($select);
+
+                    // ARMAZENA A 1ª LINHA DO BD DENTRO DA VARIÁVEL RESULTADO //
+                    while ($resultado = $query->fetch_assoc()) {
+
+                        ?>
+                    
+                        <tr>
+                          <th scope="row"> <?php echo $resultado ['id_user']?></th>
+                                      <td> <?php echo $resultado ['nm_user']?></td>
+                                      <td> <?php echo $resultado ['email']?></td>
+                                      <td> <?php echo $resultado ['setor']?></td>
+                                      <td> <a href="delete_user.php?codigo=<?php echo $resultado ['id_user']?>"><i class="fa-solid fa-trash-can"></i></td>
+                                      <td> <a href="update_user.php?codigo=<?php echo $resultado ['id_user']?>"><i class="fa-solid fa-user-pen"></i></td>
+                    
+                        </tr>
+                    
+                        <?php  };    ?>
 
 
-
-
-// chamando o arquivo de conexão com o banco de dados (é como se eu desse ctrl c e ctrl v no código da conexão.php//
-
-include 'conexao.php';
-
-
-$insert = "INSERT INTO tb_user VALUES (null, '$nome', '$email', '$setor', '$senha')";
-
-$query = $conexao ->query($insert);
-
-if ($conexao ->query($insert) === TRUE) {
-    echo "<script>alert('Usuário Cadastrado com Sucesso!');
-    location.href = '../cadastro.html'</script>";
-}
-
-
-?>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
